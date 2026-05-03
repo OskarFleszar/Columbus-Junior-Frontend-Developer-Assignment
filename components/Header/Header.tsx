55fdd2c6e62500bd4ed1177caf4acbb9ffcc2bed
Oskar Fleszar <mroski2407@gmail.com>
2026-05-03 23:43:12 +0200

    feat: header with logo and interactive cart icon

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,3 +1,26 @@
-export default function Header() {
-  return <header>TODO: Header</header>;
+import Image from "next/image";
+import CartIcon from "../CartIcon/CartIcon";
+import styles from "./Header.module.css";
+
+type HeaderProps = {
+  title: string;
+  logo: { url: string; altText: string };
+};
+
+export default function Header({ title, logo }: HeaderProps) {
+  return (
+    <header className={styles.header}>
+      <div className={styles.brand}>
+        <Image
+          src={logo.url}
+          alt={logo.altText}
+          width={160}
+          height={60}
+          priority
+        />
+        <h1 className={styles.title}>{title}</h1>
+      </div>
+      <CartIcon />
+    </header>
+  );
 }
