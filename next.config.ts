611485aab6f55a67dd53cef807577aaa44ba84bf
Oskar Fleszar <mroski2407@gmail.com>
2026-05-03 18:04:10 +0200

    chore: configure next/image remote patterns

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,8 @@ const nextConfig = {
   allowedDevOrigins: ["172.16.1.175"],
   images: {
     remotePatterns: [
-      // twoje już istniejące
+      { protocol: "https", hostname: "dummyimage.com" },
+      { protocol: "https", hostname: "images.unsplash.com" },
     ],
   },
 };
